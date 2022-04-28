9ba88776834a92c7588e21bfe45761eb38956b9d
Adam DePaolo <amdepaolo@gmail.com>
2022-04-28 18:49:44 -0400

    commit tree 3e537345e804634d6c3bb475f93552f0f488cad9

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let forwardArray = word.split('');
+  let reverseArray = [...forwardArray];
+  reverseArray.reverse();
+  reverseWord = reverseArray.join('');
+  return word === reverseWord
 }
 
 /* 
   Add your pseudocode here
+split word into array
+create second array using spread operator
+reverse second array
+join letters of second array into reverse word
+compare word to reverse word
 */
 
 /*
   Add written explanation of your solution here
+  The function turns the word into an array, then creates a second array that is the reverse of the first array, then creates a new string from the second array and compares it with the original word
 */
 
 // You can run `node index.js` to view these console logs
@@ -16,7 +27,8 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome('mississippi'))
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
